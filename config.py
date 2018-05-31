39e74b1b506721e33bb6ddd2ac1b84dbcc930a1c
Vashisth Sharma <sharmavashisth@gmail.com>
2018-05-31 10:37:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #Twitter Key
 
-consumer_key = 'XYsmCYnSS9rlpqnbiCt2OqPsT'
-consumer_secret = 'mA002UKUmV7PD97o7uUVXNQg8ih2LpIyxEwxPtvoAcEmCiVobO'
-access_token = '114762424-5VjrMbF34IqPVZolqUEZjEoLUkuukgCpA7FmuFUZ'
-access_token_secret = 's3U5K9DdOx1uhJMysbdsFypC6qAeK42B5Ns4qh7nIiCqg'
+consumer_key = 'bbbb'
+consumer_secret = 'aaaaaa'
+access_token = 'zzzzzz'
+access_token_secret = 'yyyyyy'
 
 #Weather Key
 
-wkey = "feb3a00c4642541ed7a27c4fc4f47f48"
+wkey = "xxxxxxx"
